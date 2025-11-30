eecad6ef9ae0bc3789383871933f9f8c9d7cdd16
Your Name <you@example.com>
2025-11-30 12:19:42 +0000

    TAsk 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """
-This module defines a Square class.
+this module defines a class Square
 """
 
+
 class Square:
-    """Defines a square"""
+    """
+    it represents a square shape
+    attributes:
+        __size (int): the size of the square (private).
+    """
     def __init__(self, size):
-         # Assigning the incoming 'size' to the private attribute '__size'
+        """
+        initializes a new Square instance.
+
+        args:
+            size (any): the size of the square. No validation is done yet.
+        """
+        # Assigning the incoming 'size' to the private attribute '__size'
         # the double underscore (__) tells python: "keep this hidden!"
         self.__size = size
