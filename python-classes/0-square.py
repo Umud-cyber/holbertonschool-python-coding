da489ce6efe595422fb039762584e325afe809f2
Your Name <you@example.com>
2025-11-30 12:16:33 +0000

    TAsk 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
 class Square:
+    """Defines a square"""
     def __init__(self, size):
         self.__size = size
