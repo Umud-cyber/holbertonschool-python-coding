215ecb455d6be04228fa020e9be29539ca31cb3b
Your Name <you@example.com>
2025-11-30 12:28:39 +0000

    Task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
 class Square:
+    """Defines a square with size validation."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): Size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is < 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
-    self.__size = size
+
+        self.__size = size
