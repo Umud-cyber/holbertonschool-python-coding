c69220f26aaa05d237ae631264e8ca8d4ec1174a
Your Name <you@example.com>
2025-11-30 12:57:02 +0000

    Task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """Defines a square with a private size and proper getters/setters."""
+
+    def __init__(self, size=0):
+        self.size = size  # setter çağırılır, yoxlamalar işə düşür
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size * self.__size
